8fd499c183e604ea261510205245e1af25320d6e
brightech <brightech@brightechnoMacBook-Air.local>
2023-08-23 19:04:06 +0900

    ボタンの追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter, Nothing_You_Could_Do } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import { test } from 'node:test'
 import { useState } from 'react'
 
@@ -15,22 +15,44 @@ export default function Home() {
     if (newItem.trim() !== '') {
       setStringArray(prevArray => [...prevArray, newItem]);
       setNewItem(''); // 入力フィールドをクリア
+
+      // const deleteTodo = (index: number) => {
+      //   const newTodos 
+      // }
     }
+
   }
+  // const deleteTodo = (index: number) => {
+  //   const newTodos = todos.filter((todo, todoIndex) => {
+  //     return index !== todoIndex;
+  //   });
+
+  //   setTodos(newTodos);
+  // };
+
+  const test_5 = () =>{
+    window.confirm("削除しますか？");
+  }
+
 
   return (
     <div>
-      <input type="text" 
-      onChange={event => setNewItem(event.target.value)}/>
+      <input type="text"
+        onChange={event => setNewItem(event.target.value)} />
 
-      <button onClick={() => submit()}>完了</button>
-      
-      <div>
+      <button onClick={() => submit()}>送信</button>
+
+      <div className={styles.container}>
         {stringArray.map((item, index) => (
-          <p key={index}>{item}</p>
-        ))}
+          <div>
+            <p className={styles.para} key={index}>{item}
+            </p>
+            <button onClick={() => test_5()}>完了</button>
+          </div>
+        ))
+        }
+
       </div>
     </div>
   )
 }
-
