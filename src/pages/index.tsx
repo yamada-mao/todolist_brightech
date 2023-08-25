134fc3acd8c4de5187c8435fec1f701de63378b8
brightech <brightech@brightechnoMacBook-Air.local>
2023-08-25 14:18:05 +0900

    ３つのタブに分ける

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,126 @@
 import { useState } from 'react';
+import styles from '@/styles/Home.module.scss';
 
 type Todo = {
   value: string;
   readonly id: number;
   removed: boolean;
+  comment: string;
 };
 
-type Filter = "all" | "removed";
+type Filter = "all" | "completed" | "incomplete"; // フィルターオプションを追加
 
 const App = () => {
   const [text, setText] = useState('');
-  const [detail, setDetail] = useState('');
+  const [comment, setComment] = useState('');
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<Filter>("all");
 
   const handleOnSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (text === "") {
+    if (text === "" || comment === "") {
       return;
     }
     const newTodo: Todo = {
       value: text,
       id: new Date().getTime(),
       removed: false,
+      comment: comment,
     };
     setTodos([newTodo, ...todos]);
-    //フォームへの入力をクリアする
     setText('');
+    setComment('');
   };
 
-  // formに入力更新された時更新する関数
+  const filteredTodos = todos.filter((todo) => {
+    switch (filter) {
+      case "all":
+        return true;
+      case "completed":
+        return todo.removed;
+      case "incomplete":
+        return !todo.removed;
+      default:
+        return true;
+    }
+  });
+
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
 
   const handleOnRemove = (id: number) => {
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, removed: !todo.removed };
+      }
+      return todo;
+    });
     setTodos(updatedTodos);
+  };
+
+  const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
   };
 
   return (
     <div className="w-full mx-auto max-w-2xl my-6 px-3">
       <form onSubmit={handleOnSubmit}>
-        <input
-          className="border border-black"
-          type="text"
-          value={text}
-          onChange={handleOnChange}
-        />
-        <p>詳細</p>
-        <textarea name="comment" ></textarea>
-        <button type="submit">追加</button>
+        <div className={styles.flex}>
+          <input
+            className="border border-black"
+            type="text"
+            value={text}
+            onChange={handleOnChange}
+          />
+          <button type="submit">追加</button>
+          <p>詳細</p>
+          <textarea
+            name="comment"
+            value={comment}
+            onChange={handleCommentChange}
+          ></textarea>
+        </div>
       </form>
-      <ul>
-        {todos.map((todo) => (
+      <ol>
+        <p>ALL</p>
+        {filteredTodos.map((todo) => (
           <li key={todo.id}>
             {todo.value}
             <button onClick={() => handleOnRemove(todo.id)}>
               {todo.removed ? "未完了" : "完了"}
             </button>
+            詳細: {todo.comment}
           </li>
         ))}
-      </ul>
+      </ol>
+
+      <ol>
+        <p>完了</p>
+        {filteredTodos
+          .filter((todo) => todo.removed)
+          .map((todo) => (
+            <li key={todo.id}>
+              {todo.value}
+              <button onClick={() => handleOnRemove(todo.id)}>完了</button>
+            </li>
+          ))}
+      </ol>
+
+      <ol>
+        <p>未完了</p>
+        {filteredTodos
+          .filter((todo) => !todo.removed)
+          .map((todo) => (
+            <li key={todo.id}>
+              {todo.value}
+              <button onClick={() => handleOnRemove(todo.id)}>未完了</button>
+            </li>
+          ))}
+      </ol>
     </div>
   );
 };
 
 export default App;
-
 
