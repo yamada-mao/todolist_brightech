fe346007d91b28f5ab33c708b41e37b375d155a8
brightech <brightech@brightechnoMacBook-Air.local>
2023-08-25 10:40:31 +0900

    詳細ボックスの追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,17 +10,14 @@ type Filter = "all" | "removed";
 
 const App = () => {
   const [text, setText] = useState('');
+  const [detail, setDetail] = useState('');
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [filter, setFilter] = useState<Filter>("all");
 
-  // todosステイトを更新する関数
-  const handleOnSubmit = () => {
-    // 何も入力されていない時
+  const handleOnSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
     if (text === "") {
-      alert("文字を入力してください");
-    return;
+      return;
     }
-    //新しい Todo を作成 
     const newTodo: Todo = {
       value: text,
       id: new Date().getTime(),
@@ -36,198 +33,38 @@ const App = () => {
     setText(e.target.value);
   };
 
-  // 削除ボタンがクリックされた時更新する関数
-  const handleOnRemove = (id: number, removed: boolean) => {
-    const deepCopy: Todo[] = JSON.parse(JSON.stringify(todos));
-    const newTodos = deepCopy.map((todo) => {
-      if (todo.id === id) {
-        todo.removed = !removed;
-      }
-      return todo;
-    });
-    setTodos(newTodos);
+  const handleOnRemove = (id: number) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
   };
-
-  // ゴミ箱を空にする時更新する関数
-  const handleOnEmpty = () => {
-    const newTodos = todos.filter((todo) => !todo.removed);
-    setTodos(newTodos);
-  };
-
-  // フィルター
-  const filteredTodos = todos.filter((todo) => {
-    switch (filter) {
-      case "all":
-        return !todo.removed;
-      case "removed":
-        return todo.removed;
-      default:
-        return todo;
-    }
-  });
 
   return (
     <div className="w-full mx-auto max-w-2xl my-6 px-3">
-      <select
-        defaultValue="all"
-        onChange={(e) => setFilter(e.target.value as Filter)}
-      >
-        <option value="all">すべてのタスク</option>
-        <option value="removed">ゴミ箱</option>
-      </select>
-      {filter === "removed" ? (
-        <button
-          onClick={handleOnEmpty}
-          disabled={todos.filter((todo) => todo.removed).length === 0}
-        >
-        ゴミ箱を空にする
-        </button>
-      ) : (
-      <form 
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleOnSubmit();
-        }}
-      >
-        <input  
-          className="border border-black" 
-          type="text" 
-          value={text} 
-          onChange={(e) => handleOnChange(e)} />
+      <form onSubmit={handleOnSubmit}>
         <input
-          type="submit"
-          value="追加"
-          onSubmit={handleOnSubmit}
+          className="border border-black"
+          type="text"
+          value={text}
+          onChange={handleOnChange}
         />
+        <p>詳細</p>
+        <textarea name="comment" ></textarea>
+        <button type="submit">追加</button>
       </form>
-      )}
       <ul>
-        {filteredTodos.map((todo) => {
-          return (
+        {todos.map((todo) => (
           <li key={todo.id}>
             {todo.value}
-            <button onClick={() => handleOnRemove(todo.id, todo.removed)}>
-              {todo.removed ? "復元" : "削除"}
+            <button onClick={() => handleOnRemove(todo.id)}>
+              {todo.removed ? "未完了" : "完了"}
             </button>
           </li>
-          );
-        })}
+        ))}
       </ul>
     </div>
   );
 };
+
 export default App;
 
-
-
-
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import { Inter, Nothing_You_Could_Do } from 'next/font/google'
-// import styles from '../styles/Home.module.scss'
-// import { test } from 'node:test'
-// import { useState } from 'react'
-
-// const inter = Inter({ subsets: ['latin'] })
-
-
-
-
-
-// export default function Home() {
-//   const [stringArray, setStringArray] = useState(['掃除する', '勉強する', '貯金する']);
-//   const [newItem, setNewItem] = useState('');
-
-
-//   type Todo = {
-//     value: string;
-//     readonly id: number;
-//     checked: boolean;
-//     removed: boolean;
-//   };
-
-//   // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//   //   setStringArray(e.target.value);
-//   // };
-
-
-//   const submit = () => {
-
-//     // const newTodo: Todo = {
-//     //   id: new Date().getTime(),
-//     //   checked: false,
-//     //   removed: false,
-//     //   value: ''
-//     // };
-//     if (newItem.trim() !== '') {
-//       setStringArray(prevArray => [...prevArray, newItem]);
-//       // useState('')
-//       setNewItem(""); // 入力フィールドをクリア
-
-//       // const deleteTodo = (index: number) => {
-//       //   const newTodos 
-//       // }
-//     }
-
-//   }
-
-
-
-//   const deleteTodo = (item: string, index: number) => {
-//     // window.confirm();
-//     console.log(11111);
-//     console.log(stringArray);
-//     console.log(22222);
-//   }
-
-//   // const handleRemove = (id: number, removed: boolean) => {
-//   //   setTodos((todos: any[]) => {
-//   //     const newTodos = todos.map((todo: { id: number }) => {
-//   //       if (todo.id === id) {
-//   //         return { ...todo, removed };
-//   //       }
-//   //       return todo;
-//   //     });
-
-//   //     return newTodos;
-//   //   });
-//   // };
-
-//   return (
-//     <div>
-//       <input type="text"
-//         onChange={(event) => setNewItem(event.target.value)} />
-
-//       <button onClick={() => submit()}>送信</button>
-
-//       <div className={styles.container}>
-//         {stringArray.map((item, index) => (
-//           <div>
-//             <p className={styles.para} key={index}>{item}
-//             </p>
-//             <button onClick={() => deleteTodo(item, index)}>完了</button>
-//           </div>
-//         ))
-//         }
-
-//       </div>
-//     </div>
-//   )
-// }
-
-// const deleteTodo = () => {
-
-// }
-
-
-
-
-// // function setTodos(arg0: (todos: any[]) => ({ id: number } | { removed: boolean; id: number })[]) {
-// //   throw new Error('Function not implemented.')
-// // }
-
-// // function setTodos(arg0: (todos: any) => any[]) {
-// //   // throw new Error('Function not implemented.');
-
-// // }
 
